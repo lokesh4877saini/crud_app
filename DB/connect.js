370c6d9aa6ae8e851e8ede94d550247c57400a6c
lokesh4877saini <genu08saini@gmail.com>
2024-08-08 23:08:20 +0530

    adding app.js

--- DB/connect.js
+++ DB/connect.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const db = process.env.dburl;
-mongoose.connect(db,{ }
-    ).then(() =>console.log("connection with db success")).catch((e)=>console.log("connection fail",e));
+const db = process.env.dbUrl;
+mongoose.connect(db).then(() =>console.log("connection with db success")).catch((e)=>console.log("connection fail",e));
