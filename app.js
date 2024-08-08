22a978e351eb0961873f84e98ae5cce7037ba491
lokesh4877saini <genu08saini@gmail.com>
2024-08-08 23:00:31 +0530

    new commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
+require('dotenv').config();
 const express = require('express')
 const app = express();
 const router = require('./Routes/router');
 const cors = require('cors');
-require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 require('./DB/connect')
 const port = process.env.PORT || 5000;
 app.use(router);
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is http://localhost:${port}`)
 })
